4347af9824948b0e5a883851c3ecbe086dc2b929
Adrian-P0gar <pogar.adi@gmail.com>
2019-11-13 10:30:05 +0200

    resolve, resolve the conflict

--- gittie.py
+++ gittie.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-<< << << < HEAD
-
 
 def introduce():
     print("Hello, I'm Gittie!")
-    print("Gittie are just an animal.")
+    print("Gittie are just an animals")
 
 
 def joke(punch_line):
@@ -12,21 +9,8 @@ def joke(punch_line):
     print(punch_line)
 
 
-== == == =
-
-=======
-<<<<<<< HEAD
-def introduce ():
-    print("Hello!")
->>>>>>> bf58ac8b45041a13f34b6dd7e6dad76be04233b7
-
-def introduce():
-    print("Hello, I'm Gittie!")
-
-
 def add(a, b):
     return a + b
 
 
 print(add(1, 2))
->>>>>> > 32b3664fa8d76451c9ec7a4d0a5463bcaa89e6bf
